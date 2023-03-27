f32fa78588834a3132ab15d49942635dc4864877
Chung-Po Tseng <b97609070@g.ntu.edu.tw>
2023-03-27 19:10:52 +0800

    739. Daily Temperatures

---
+++ 739. Daily Temperatures/739.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+/*
+g++ 739.cpp -Wall -O2 -std=c++17 -o 739
+
+https://leetcode.com/problems/daily-temperatures/solutions/1574806/c-easy-standard-sol-intuitive-approach-with-dry-run-stack-appraoch/
+*/
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    int tempSize {static_cast<int>(temperatures.size())};
+    if (tempSize == 1){
+        vector<int> answer {0};
+        return answer;
+    }
+
+    stack<int> temps;
+    stack<int> index;
+    vector<int> answer {0};
+
+    temps.push(temperatures.at(tempSize-1));
+    index.push(tempSize-1);
+
+    for (int i {tempSize-2}; i >= 0; --i){
+        while (temps.empty() != true && temps.top() <= temperatures.at(i)){
+            temps.pop();
+            index.pop();
+        }
+
+        if (temps.empty() == true){
+            answer.push_back(0);
+        }
+        else{
+            answer.push_back(index.top() - i);
+        }
+
+        temps.push(temperatures.at(i));
+        index.push(i);
+    }
+
+    reverse(answer.begin(), answer.end());
+    return answer;
+}
+
+void printVector(const vector<int> & vec){
+    for (auto & num: vec){
+        cout << num << " ";
+    }
+    cout << endl;
+}
+int main(){
+
+    vector<int> temps1 {73,74,75,71,69,72,76,73};
+    printVector(dailyTemperatures(temps1));
+    return 0;
+}
