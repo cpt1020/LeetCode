661e3d3083bd48b9c936f1ba5873ae674357071c
Chung-Po Tseng <b97609070@g.ntu.edu.tw>
2023-04-20 16:29:30 +0800

    207. Course Schedule

--- 207. Course Schedule/207.cpp
+++ 207. Course Schedule/207.cpp
@@ -52,6 +52,44 @@ public:
         }
         return true;
     }
+
+    bool canFinish_BFS(int numCourses, vector<vector<int>>& prerequisites) {
+    // using topological sort
+        if (prerequisites.size() == 0 || prerequisites.size() == 1){
+            return true;
+        }
+        vector<vector<int>> graph (numCourses);
+        for (int i {0}; i < prerequisites.size(); ++i){
+            graph.at(prerequisites.at(i).at(1)).push_back(prerequisites.at(i).at(0));
+        }
+
+        vector<int> in_degree (numCourses, 0);
+        for (auto node: graph) {
+            for (auto adj_n: node) {
+                in_degree.at(adj_n) += 1;
+            }
+        }
+        
+        for (int i {0}; i < numCourses; ++i){
+            int j {0};
+            for (; j < numCourses; ++j) {
+                if (in_degree.at(j) == 0) {
+                    break;
+                }
+            }
+
+            if (j == numCourses) {
+                return false;
+            }
+
+            in_degree.at(j) -= 1;
+
+            for (auto n: graph.at(j)) {
+                in_degree.at(n) -= 1;
+            }
+        }
+        return true;
+    }
 };
 
 int main(){
@@ -60,9 +98,19 @@ int main(){
 
     vector<vector<int>> prerequisites {{1,0}};
     cout << boolalpha << sol.canFinish(2, prerequisites) << endl;
+    cout << boolalpha << sol.canFinish_BFS(2, prerequisites) << endl;
 
     prerequisites = {{1,0}, {0,1}};
     cout << boolalpha << sol.canFinish(2, prerequisites) << endl;
+    cout << boolalpha << sol.canFinish_BFS(2, prerequisites) << endl;
+
+    prerequisites = {{0,10},{3,18},{5,5},{6,11},{11,14},{13,1},{15,1},{17,4}};
+    cout << boolalpha << sol.canFinish(20, prerequisites) << endl;
+    cout << boolalpha << sol.canFinish_BFS(20, prerequisites) << endl;
+
+    prerequisites = {{1,4},{2,4},{3,1},{3,2}};
+    cout << boolalpha << sol.canFinish(5, prerequisites) << endl;
+    cout << boolalpha << sol.canFinish_BFS(5, prerequisites) << endl;
 
     return 0;
 }
