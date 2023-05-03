fe2489083b6ae8879df15d6b231f8347176d09a9
Chung-Po Tseng <b97609070@g.ntu.edu.tw>
2023-05-03 11:38:35 +0800

    utils

--- utils.h
+++ utils.h
@@ -18,4 +18,15 @@ void printVector(const vector<T>& vec) {
     cout << endl;
 }
 
+template <typename T>
+void print2dVector(const vector<vector<T>>& vec) {
+    for (auto v: vec) {
+        for (auto val: v) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 #endif
