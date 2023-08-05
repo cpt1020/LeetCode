9bbbd0f6a63f0d8bd00c26b1d1af502bc08c55fd
Chung-Po Tseng <b97609070@g.ntu.edu.tw>
2023-08-06 02:37:54 +0800

    1061. Lexicographically Smallest Equivalent String

--- 1061. Lexicographically Smallest Equivalent String/1061.cpp
+++ 1061. Lexicographically Smallest Equivalent String/1061.cpp
@@ -10,45 +10,88 @@ g++ 1061.cpp -std=c++17 -o 1061 && ./1061
 class Solution {
 public:
 
-    char FindSet(unordered_map<char, char>& parent, char& c) {
-        if (parent[c] != c) {
-            parent[c] = FindSet(parent, parent[c]);
+    char FindSet(char *parent, char& c) {
+    // char FindSet(char parent [], char& c) {
+        if (parent[c - 'a'] != c) {
+            parent[c - 'a'] = FindSet(parent, parent[c - 'a']);
         }
-        return parent[c];
+
+        return parent[c - 'a'];
     }
 
-    void UnionSet(unordered_map<char, char>& parent, char& c1, char& c2) {
-        char parent1 {FindSet(parent, c1)};
-        char parent2 {FindSet(parent, c2)};
+    void UnionSet(char *parent, char& s1, char& s2) {
+    // void UnionSet(char parent [], char& s1, char& s2) {
+        char parent1 {FindSet(parent, s1)};
+        char parent2 {FindSet(parent, s2)};
 
         if (parent1 != parent2) {
-            parent[max(parent1, parent2)] = min(parent1, parent2);
+            parent[max(parent1, parent2) - 'a'] = min(parent1, parent2);
         }
     }
 
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        unordered_map<char, char> parent;
+        char parent [26];
 
-        for (char c {'a'}; c <= 'z'; ++c) {
-            parent[c] = c;
+        for (int i {0}; i < 26; ++i) {
+            parent[i] = i + 'a';
         }
 
         for (int i {0}; i < s1.size(); ++i) {
             UnionSet(parent, s1.at(i), s2.at(i));
         }
 
+        // 做最後一次
+        for (char c {'a'}; c <= 'z'; ++c) {
+            parent[c - 'a'] = FindSet(parent, parent[c - 'a']);
+        }
+
         string ans;
-        for (auto& c: baseStr) {
-            if (parent.find(c) != parent.end()) {
-                ans += FindSet(parent, c);
-            }
-            else {
-                ans += c;
-            }
+        for (const auto& c : baseStr) {
+            ans += parent[c  - 'a'];
         }
 
         return ans;
     }
+
+    // char FindSet(unordered_map<char, char>& parent, char& c) {
+    //     if (parent[c] != c) {
+    //         parent[c] = FindSet(parent, parent[c]);
+    //     }
+    //     return parent[c];
+    // }
+
+    // void UnionSet(unordered_map<char, char>& parent, char& c1, char& c2) {
+    //     char parent1 {FindSet(parent, c1)};
+    //     char parent2 {FindSet(parent, c2)};
+
+    //     if (parent1 != parent2) {
+    //         parent[max(parent1, parent2)] = min(parent1, parent2);
+    //     }
+    // }
+
+    // string smallestEquivalentString(string s1, string s2, string baseStr) {
+    //     unordered_map<char, char> parent;
+
+    //     for (char c {'a'}; c <= 'z'; ++c) {
+    //         parent[c] = c;
+    //     }
+
+    //     for (int i {0}; i < s1.size(); ++i) {
+    //         UnionSet(parent, s1.at(i), s2.at(i));
+    //     }
+
+    //     string ans;
+    //     for (auto& c: baseStr) {
+    //         if (parent.find(c) != parent.end()) {
+    //             ans += FindSet(parent, c);
+    //         }
+    //         else {
+    //             ans += c;
+    //         }
+    //     }
+
+    //     return ans;
+    // }
 };
 
 int main() {
@@ -56,8 +99,11 @@ int main() {
     Solution sol;
 
     cout << sol.smallestEquivalentString("hello", "world", "hold") << endl;
+    // "hdld"
     cout << sol.smallestEquivalentString("parker", "morris", "parser") << endl;
+    // "makkek"
     cout << sol.smallestEquivalentString("leetcode", "programs", "sourcecode") << endl;
+    // "aauaaaaada"
 
     return 0;
 }
